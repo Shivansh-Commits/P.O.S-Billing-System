2cea6315b611c8fb26eb028e88e3e0d4d113537c
Shivansh Singh <shivanshbhadouria7@gmail.com>
2020-02-27 02:57:22 +0530

    Add files via upload

--- about_page.java
+++ about_page.java
@@ -13,6 +13,11 @@ import javax.swing.border.BevelBorder;
 import java.awt.Color;
 import javax.swing.UIManager;
 import javax.swing.border.LineBorder;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JTextField;
+import javax.swing.DropMode;
+import javax.swing.JTextPane;
 
 public class about_page extends JFrame {
 
@@ -39,24 +44,36 @@ public class about_page extends JFrame {
 	 */
 	public about_page() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 430, 377);
 		contentPane = new JPanel();
 		contentPane.setBackground(SystemColor.activeCaption);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new LineBorder(new Color(0, 102, 153), 2, true));
+		panel_1.setBounds(74, 77, 267, 245);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(6, 15, 255, 224);
+		panel_1.add(scrollPane);
+		
+		JTextPane txtpnThisIsA = new JTextPane();
+		scrollPane.setViewportView(txtpnThisIsA);
+		txtpnThisIsA.setText(" This is a SuperMarket Managment System.\r\nThere are Two Types of Users who can acces this Software\r\n  1.Administrator\r\n  2.Cashier\r\n\r\n1).Functionalities Provided to Administrator:\r\n\r\na) USER\r\n  \t1.Add Cashier Account\r\n  \t2.Remove Cashier Account\r\n \t3.View Cashier Details\r\n\t4.View User Logs\r\n  \t5.Log Out\r\n\r\nb) INVENTORY\r\n\t1.Add Product \r\n\t2.Remove Product\r\n\t3.Delete Product\r\n\t4.Modify Product Details\r\n      5.Open Inventory\r\n\r\nc) PURCHASE\r\n\t1.Generate Bill\r\n\t2.Sales Logs\r\n\t3.Customer Information\r\n\r\nd) TAX \r\n\t1.Tax Details\r\n\t2Update Tax Details\r\n\r\ne) ABOUT\r\n\r\n2).Functionalities Given to the Cashier:\r\n\r\na) USER\r\n  \t1.LOG OUT\r\n  \t\t\r\nb) INVENTORY\r\n\t1.Add Product \r\n\t2.Remove Product\r\n\t3.Delete Product\r\n\t4.Modify Product Details\r\n     \t5.Open Inventory\r\n\r\nc) PURCHASE\r\n\t1.Generate Bill\r\n\t2.Customer Information\r\n\r\nd) ABOUT\r\n\r\nDeveloped By Shivansh Singh Bhadouria.\r\nB-Tech CS(Hons)");
+		
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(240, 240, 240));
-		panel.setBorder(new LineBorder(Color.CYAN, 4, true));
-		panel.setBounds(40, 73, 350, 82);
+		panel.setBorder(new LineBorder(new Color(0, 102, 153), 2, true));
+		panel.setBounds(171, 15, 64, 35);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("This is a SuperMarket Managment System.");
-		lblNewLabel.setBounds(10, 10, 338, 61);
+		JLabel lblNewLabel = new JLabel("ABOUT");
+		lblNewLabel.setBounds(10, 0, 52, 35);
 		panel.add(lblNewLabel);
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 13));
 	}
-
 }
