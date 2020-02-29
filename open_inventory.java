24be5ccc75d60bc18c9d717487a5da2057ec7f3d
Shivansh Singh <shivanshbhadouria7@gmail.com>
2020-02-29 15:17:01 +0530

    Add files via upload

---
+++ open_inventory.java
@@ -0,0 +1,88 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.SystemColor;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EtchedBorder;
+
+public class open_inventory extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+	private JLabel lblNewLabel;
+	private JPanel panel;
+	private JPanel panel_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					open_inventory frame = new open_inventory();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public open_inventory() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 870, 430);
+		contentPane = new JPanel();
+		contentPane.setBackground(SystemColor.activeCaption);
+		contentPane.setForeground(SystemColor.activeCaption);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		panel_1 = new JPanel();
+		panel_1.setBackground(new Color(0, 102, 153));
+		panel_1.setBorder(new CompoundBorder(new LineBorder(new Color(64, 64, 64), 1, true), new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Inventory", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0))));
+		panel_1.setBounds(26, 67, 794, 280);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 23, 774, 247);
+		panel_1.add(scrollPane);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+		
+		insertdataindb open = new insertdataindb();
+		open.open(table);
+		panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(0, 102, 153), 2, true));
+		panel.setBounds(373, 10, 94, 41);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		lblNewLabel = new JLabel("Inventory");
+		lblNewLabel.setBounds(10, 10, 81, 21);
+		panel.add(lblNewLabel);
+		lblNewLabel.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 18));
+		
+	}
+
+}
