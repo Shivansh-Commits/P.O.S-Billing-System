dad4dc3620f8eace27412f11128279989e0ccb6e
Shivansh Singh <shivanshbhadouria7@gmail.com>
2020-03-04 15:17:42 +0530

    Add files via upload

--- Admin_Menu.java
+++ Admin_Menu.java
@@ -3,6 +3,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
@@ -10,6 +11,7 @@ import javax.swing.JMenuItem;
 import java.awt.SystemColor;
 import java.awt.Font;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.ActionEvent;
 
 import javax.imageio.ImageIO;
@@ -40,15 +42,18 @@ public class Admin_Menu extends JFrame {
 	 * Create the frame.
 	 */
 	public Admin_Menu() {
+		
+		
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 648, 388);
+		setBounds(100, 100, 645, 385);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
 		JMenu mnNewMenu = new JMenu("Users");
-		mnNewMenu.setIcon(new ImageIcon("D:\\Lawrenciums\\College Stuff\\4 SEM\\JAVA\\Icons\\User.png"));
-		mnNewMenu.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		mnNewMenu.setIcon(new ImageIcon("D:\\Lawrenciums\\College Stuff\\4 SEM\\JAVA\\Icons\\icons8-customer-16.png"));
+		mnNewMenu.setFont(new Font("Microsoft Yi Baiti", Font.BOLD, 17));
 		menuBar.add(mnNewMenu);
 		
 		JMenuItem mntmNewMenuItem_10 = new JMenuItem("Add Cashier Account");
@@ -106,7 +111,7 @@ public class Admin_Menu extends JFrame {
 		
 		JMenu mnNewMenu_1 = new JMenu("Inventory");
 		mnNewMenu_1.setIcon(new ImageIcon("D:\\Lawrenciums\\College Stuff\\4 SEM\\JAVA\\Icons\\Inventory.png"));
-		mnNewMenu_1.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		mnNewMenu_1.setFont(new Font("Microsoft Yi Baiti", Font.BOLD, 17));
 		menuBar.add(mnNewMenu_1);
 		
 		JMenuItem mntmNewMenuItem_9 = new JMenuItem("Open Inventory");
@@ -163,7 +168,7 @@ public class Admin_Menu extends JFrame {
 		
 		JMenu mnNewMenu_2 = new JMenu("Purchase");
 		mnNewMenu_2.setIcon(new ImageIcon("D:\\Lawrenciums\\College Stuff\\4 SEM\\JAVA\\Icons\\Purchase.png"));
-		mnNewMenu_2.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		mnNewMenu_2.setFont(new Font("Microsoft Yi Baiti", Font.BOLD, 17));
 		menuBar.add(mnNewMenu_2);
 		
 		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Generate Bill");
@@ -191,23 +196,13 @@ public class Admin_Menu extends JFrame {
 		mntmNewMenuItem_3.setIcon(new ImageIcon("D:\\Lawrenciums\\College Stuff\\4 SEM\\JAVA\\Icons\\icons8-land-sales-16.png"));
 		mnNewMenu_2.add(mntmNewMenuItem_3);
 		
-		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Customer Information");
-		mntmNewMenuItem_4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				customer_info show_cust_info = new customer_info();
-				show_cust_info.setVisible(true);
-				
-			}
-		});
-		mntmNewMenuItem_4.setIcon(new ImageIcon("D:\\Lawrenciums\\College Stuff\\4 SEM\\JAVA\\Icons\\cutomer info.png"));
-		mnNewMenu_2.add(mntmNewMenuItem_4);
-		
 		JMenu mnNewMenu_4 = new JMenu("Tax Details");
 		mnNewMenu_4.setIcon(new ImageIcon("D:\\Lawrenciums\\College Stuff\\4 SEM\\JAVA\\Icons\\Tax.png"));
-		mnNewMenu_4.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		mnNewMenu_4.setFont(new Font("Microsoft Yi Baiti", Font.BOLD, 17));
 		menuBar.add(mnNewMenu_4);
 		
 		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Tax Details");
+		mntmNewMenuItem_1.setIcon(new ImageIcon("D:\\Lawrenciums\\College Stuff\\4 SEM\\JAVA\\Icons\\tax details.png"));
 		mntmNewMenuItem_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -220,6 +215,7 @@ public class Admin_Menu extends JFrame {
 		mnNewMenu_4.add(mntmNewMenuItem_1);
 		
 		JMenuItem mntmNewMenuItem_15 = new JMenuItem("Update Tax Details");
+		mntmNewMenuItem_15.setIcon(new ImageIcon("D:\\Lawrenciums\\College Stuff\\4 SEM\\JAVA\\Icons\\update tax detaails.png"));
 		mntmNewMenuItem_15.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				update_tax_details_page update_tax_details = new update_tax_details_page();
@@ -229,12 +225,35 @@ public class Admin_Menu extends JFrame {
 		});
 		mnNewMenu_4.add(mntmNewMenuItem_15);
 		
+		JMenu mnNewMenu_5 = new JMenu("Customer Credentials");
+		mnNewMenu_5.setIcon(new ImageIcon("D:\\Lawrenciums\\College Stuff\\4 SEM\\JAVA\\Icons\\Customer _ info.png"));
+		mnNewMenu_5.setFont(new Font("Microsoft Yi Baiti", Font.BOLD, 17));
+		menuBar.add(mnNewMenu_5);
+		
+		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Customer Information");
+		mnNewMenu_5.add(mntmNewMenuItem_4);
+		mntmNewMenuItem_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				customer_info show_cust_info = new customer_info();
+				show_cust_info.setVisible(true);
+				
+			}
+		});
+		mntmNewMenuItem_4.setIcon(new ImageIcon("D:\\Lawrenciums\\College Stuff\\4 SEM\\JAVA\\Icons\\cutomer info.png"));
+		
 		JMenu mnNewMenu_3 = new JMenu("About");
 		mnNewMenu_3.setIcon(new ImageIcon("D:\\Lawrenciums\\College Stuff\\4 SEM\\JAVA\\Icons\\icons8-information-16.png"));
-		mnNewMenu_3.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		mnNewMenu_3.setFont(new Font("Microsoft Yi Baiti", Font.BOLD, 17));
 		menuBar.add(mnNewMenu_3);
 		
 		JMenuItem mntmNewMenuItem = new JMenuItem("About");
+		mntmNewMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				about_page about = new about_page();
+				about.setVisible(true);
+			}
+		});
 		mntmNewMenuItem.setIcon(new ImageIcon("D:\\Lawrenciums\\College Stuff\\4 SEM\\JAVA\\Icons\\icons8-information-16.png"));
 		mnNewMenu_3.add(mntmNewMenuItem);
 		
@@ -246,8 +265,64 @@ public class Admin_Menu extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon("D:\\Lawrenciums\\College Stuff\\4 SEM\\JAVA\\Icons\\cup-of-coffee-1280537_640.jpg"));
-		lblNewLabel.setBounds(0, -90, 634, 522);
+		lblNewLabel.setIcon(new ImageIcon("D:\\Lawrenciums\\College Stuff\\4 SEM\\JAVA\\Icons\\skyscraper-1149478_640.jpg"));
+		lblNewLabel.setBounds(0, -87, 634, 522);
 		contentPane.add(lblNewLabel);
+		
+		mnNewMenu.setMnemonic(KeyEvent.VK_Q);//Shortcut for Menu
+		mnNewMenu_1.setMnemonic(KeyEvent.VK_W);//Shortcut for Menu
+		mnNewMenu_2.setMnemonic(KeyEvent.VK_E);//Shortcut for Menu
+		mnNewMenu_3.setMnemonic(KeyEvent.VK_Y);//Shortcut for Menu
+		mnNewMenu_4.setMnemonic(KeyEvent.VK_R);//Shortcut for Menu
+		mnNewMenu_5.setMnemonic(KeyEvent.VK_T);//Shortcut for Menu
+		
+		
+		KeyStroke keyStrokeToOpen= KeyStroke.getKeyStroke(KeyEvent.VK_A,KeyEvent.CTRL_DOWN_MASK);
+		mntmNewMenuItem_10.setAccelerator(keyStrokeToOpen);
+		
+		KeyStroke keyStrokeToOpen_1= KeyStroke.getKeyStroke(KeyEvent.VK_R,KeyEvent.CTRL_DOWN_MASK);
+		mntmNewMenuItem_13.setAccelerator(keyStrokeToOpen_1);
+		
+		KeyStroke keyStrokeToOpen_2= KeyStroke.getKeyStroke(KeyEvent.VK_U,KeyEvent.CTRL_DOWN_MASK);
+		mntmNewMenuItem_12.setAccelerator(keyStrokeToOpen_2);
+		
+		KeyStroke keyStrokeToOpen_3= KeyStroke.getKeyStroke(KeyEvent.VK_D,KeyEvent.CTRL_DOWN_MASK);
+		mntmNewMenuItem_14.setAccelerator(keyStrokeToOpen_3);
+		
+		KeyStroke keyStrokeToOpen_4= KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE,0);
+		mntmNewMenuItem_11.setAccelerator(keyStrokeToOpen_4);
+		
+		KeyStroke keyStrokeToOpen_5= KeyStroke.getKeyStroke(KeyEvent.VK_F1,0);
+		mntmNewMenuItem_9.setAccelerator(keyStrokeToOpen_5);
+
+		KeyStroke keyStrokeToOpen_6= KeyStroke.getKeyStroke(KeyEvent.VK_F2,0);
+		mntmNewMenuItem_6.setAccelerator(keyStrokeToOpen_6);
+
+		KeyStroke keyStrokeToOpen_7= KeyStroke.getKeyStroke(KeyEvent.VK_F3,0);
+		mntmNewMenuItem_7.setAccelerator(keyStrokeToOpen_7);
+		
+		KeyStroke keyStrokeToOpen_8= KeyStroke.getKeyStroke(KeyEvent.VK_F4,0);
+		mntmNewMenuItem_8.setAccelerator(keyStrokeToOpen_8);
+		
+		KeyStroke keyStrokeToOpen_9= KeyStroke.getKeyStroke(KeyEvent.VK_F5,0);
+		mntmNewMenuItem_5.setAccelerator(keyStrokeToOpen_9);
+		
+		KeyStroke keyStrokeToOpen_10= KeyStroke.getKeyStroke(KeyEvent.VK_F6,0);
+		mntmNewMenuItem_2.setAccelerator(keyStrokeToOpen_10);
+		
+		KeyStroke keyStrokeToOpen_11= KeyStroke.getKeyStroke(KeyEvent.VK_F7,0);
+		mntmNewMenuItem_3.setAccelerator(keyStrokeToOpen_11);
+		
+		KeyStroke keyStrokeToOpen_12= KeyStroke.getKeyStroke(KeyEvent.VK_F8,0);
+		mntmNewMenuItem_1.setAccelerator(keyStrokeToOpen_12);
+		
+		KeyStroke keyStrokeToOpen_13= KeyStroke.getKeyStroke(KeyEvent.VK_F9,0);
+		mntmNewMenuItem_15.setAccelerator(keyStrokeToOpen_13);
+		
+		KeyStroke keyStrokeToOpen_14= KeyStroke.getKeyStroke(KeyEvent.VK_F10,0);
+		mntmNewMenuItem_4.setAccelerator(keyStrokeToOpen_14);
+		
+		
+
 	}
 }
